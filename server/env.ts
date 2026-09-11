adba1ab84111cbee8125317b2ed8909fbca579f4
MenelaosI <menelaos.ioannou@gmail.com>
2026-09-11 08:58:05 -0300

    Adds quiet to env.ts

--- server/env.ts
+++ server/env.ts
@@ -3,4 +3,4 @@
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'node:url';
 
-dotenv.config({ path: fileURLToPath(new URL('../.env', import.meta.url)) });
+dotenv.config({ path: fileURLToPath(new URL('../.env', import.meta.url)), quiet: true });
